653f83c4e1d5e51d76e9d8eb8665007ddb00f413
jay ess <mentoce@gmail.com>
2023-02-22 00:18:04 -0800

    Give types in docstrings

--- albatross/posts_manager.py
+++ albatross/posts_manager.py
@@ -1,3 +1,9 @@
+    """
+    _summary_
+
+    Returns:
+        _type_: _description_
+    """
 from pathlib import Path
 from tempfile import NamedTemporaryFile, TemporaryFile
 
@@ -7,12 +13,12 @@ def create_post(metadata: dict, content: str, base_dir: Path) -> Path:
     Create a new (temporary) post file with the given metadata and content.
 
     Parameters:
-        metadata: A dictionary containing the metadata for the post.
-        content: The content of the post, as a string.
-        base_dir: The base directory where the post file should be created.
+        metadata (dict): The metadata for the post.
+        content (str): The content of the post.
+        base_dir (Path): The base directory where the post file will be created.
 
     Returns:
-        The path to the temporary post file
+        Path: path to the temporary post file
     """
     tf = NamedTemporaryFile(dir=base_dir, suffix=".md", mode="w", delete=False)
     tf.write("---\n")
