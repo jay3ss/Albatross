81b90065c90bb05b1f7a07303d7e3105b3e664b0
jay ess <mentoce@gmail.com>
2023-02-21 23:56:42 -0800

    Test API properly

--- tests/test_posts_manager.py
+++ tests/test_posts_manager.py
@@ -1,26 +1,28 @@
 import datetime
 
-from .fixtures import base_dir
 import albatross.posts_manager as pm
 
 
 
-def test_create_post(base_dir):
+def test_create_post(tmpdir):
     # Create a dictionary with the metadata for the post
     metadata = {'title': 'Test post', 'author': 'Test Author'}
     # Create a string with the content for the post
     content = 'This is a test post'
     # Call the create_post function and store the result in a variable
-    post_path = pm.create_post(metadata, content, base_dir)
+    post_file = pm.create_post(metadata, content, tmpdir)
     # Use the assert function to check that the returned path exists and is a file
-    assert post_path.exists() and post_path.is_file()
+    assert post_file.exists() and post_file.is_file()
     # Use the assert function to check that the metadata and content of the
     # created post file are correct
-    assert post_path.read_text() == content
-    assert post_path.metadata == metadata
+    post_content = "---\n"
+    post_content += "\n".join([f"{key}: {value}" for key, value in metadata.items()])
+    post_content += f"\n---\n\n{content}"
+    assert post_file.read_text() == post_content
 
 
-def test_create_post(base_dir):
+
+def test_create_post_metadata(tmpdir):
     metadata = {
         "title": "My first post",
         "author": "John Smith",
@@ -33,13 +35,11 @@ def test_create_post(base_dir):
     }
     content = "This is the content of my first post"
 
-    post_path = pm.create_post(metadata, content, base_dir)
+    post_path = pm.create_post(metadata, content, tmpdir)
 
-    assert post_path.is_file()
-    assert post_path.read_text() == content
-    assert metadata["slug"] in post_path.name
-    assert metadata["created_at"].strftime("%Y-%m-%d") in post_path.name
+    post_content = "---\n"
+    post_content += "\n".join([f"{key}: {value}" for key, value in metadata.items()])
+    post_content += f"\n---\n\n{content}"
 
-    with open(post_path, "r") as f:
-        file_content = f.read()
-        assert file_content == f"---\n{metadata}\n---\n\n{content}"
+    assert post_path.name[-3:] == ".md"
+    assert post_content == post_path.read_text()
