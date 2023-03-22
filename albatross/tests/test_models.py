6b3480f1db1ee68c347cb4af18c146332e9fe986
jay ess <mentoce@gmail.com>
2023-03-21 19:07:58 -0700

    Reorganize to be more MVC-like (source: https://stackoverflow.com/a/64987404)

--- albatross/tests/test_models.py
+++ albatross/tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
-from albatross.database.models import Author, Base, Post
+from albatross.core.models.models import Author, Base, Post
 
 
 @pytest.fixture
