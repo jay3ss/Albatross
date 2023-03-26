50416b95fb7470bfbed2cb439d8ec1faa706cc9e
jay ess <mentoce@gmail.com>
2023-03-26 15:00:34 -0700

    Write test for getting an article by its ID

--- albatross/tests/test_db_helpers.py
+++ albatross/tests/test_db_helpers.py
@@ -4,3 +4,7 @@ from albatross.tests.fixtures import in_memory_prepopulated_db
 
 def test_get_article(in_memory_prepopulated_db):
     temp_db = in_memory_prepopulated_db
+    article = db.get_article_by_id(article_id=1, db=temp_db)
+    assert article.id == 1
+    assert article.title == "Article 1"
+    assert article.author.name == "Author 1"
