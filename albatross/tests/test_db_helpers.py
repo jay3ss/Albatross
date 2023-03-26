91a501cf536b795d7530b3cd250965a42c7c72f3
jay ess <mentoce@gmail.com>
2023-03-26 15:48:42 -0700

    Update test name

--- albatross/tests/test_db_helpers.py
+++ albatross/tests/test_db_helpers.py
@@ -2,7 +2,7 @@ from albatross.helpers import database as db
 from albatross.tests.fixtures import in_memory_prepopulated_db
 
 
-def test_get_article(in_memory_prepopulated_db):
+def test_get_article_by_id(in_memory_prepopulated_db):
     temp_db = in_memory_prepopulated_db
     article = db.get_article_by_id(article_id=1, db=temp_db)
     assert article.id == 1
