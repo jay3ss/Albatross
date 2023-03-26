e62c50e3f99d0b3cbb67254d1954c4eba622b62b
jay ess <mentoce@gmail.com>
2023-03-26 15:39:15 -0700

    Add test for limiting results

--- albatross/tests/test_db_helpers.py
+++ albatross/tests/test_db_helpers.py
@@ -16,3 +16,11 @@ def test_get_articles(in_memory_prepopulated_db):
     assert len(articles) == 3
     for i, article in enumerate(articles, start=1):
         assert article.title == f"Article {i}"
+
+
+def test_get_articles_limited(in_memory_prepopulated_db):
+    temp_db = in_memory_prepopulated_db
+    articles = db.get_articles(limit=2, db=temp_db)
+    assert len(articles) == 2
+    for i, article in enumerate(articles, start=1):
+        assert article.title == f"Article {i}"
