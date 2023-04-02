b289991bf0c0745edb8600bf567bc385e8ca5832
jay ess <mentoce@gmail.com>
2023-04-02 13:46:50 -0700

    Start testing author show view

--- albatross/tests/test_authors_templates.py
+++ albatross/tests/test_authors_templates.py
@@ -18,6 +18,20 @@ def test_authors_index_template():
     assert "Author 2 <em>0 articles</em>"
 
 
+def test_show_author_template():
+    template = jinja2.Environment(
+        loader=jinja2.PackageLoader("albatross", "templates")
+    ).get_template("authors/show.html")
+
+    author = models.Author(name="Author 1")
+    url_for = app.url_path_for
+    rendered = template.render(author=author, url_for=url_for)
+
+    assert f"<title>{author.name}</title>" in rendered
+    assert f"<h1>{author.name}</h1>"
+    assert f"{author.name} has <em>0 articles</em>"
+
+
 if __name__ == "__main__":
     import pytest
     pytest.main(["-s", __file__])
