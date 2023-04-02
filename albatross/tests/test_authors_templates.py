e375a74498735da9823e5d134277c66b854acbe2
jay ess <mentoce@gmail.com>
2023-04-01 21:29:02 -0700

    Add test for rendering Author index template

--- albatross/tests/test_authors_templates.py
+++ albatross/tests/test_authors_templates.py
@@ -1,6 +1,7 @@
 import jinja2
 
 from albatross.core import models
+from albatross.main import app
 
 
 def test_authors_index_template():
@@ -9,8 +10,14 @@ def test_authors_index_template():
     ).get_template("authors/index.html")
 
     authors = [models.Author(name="Author 1"), models.Author(name="Author 2")]
-    rendered = template.render(context={"authors": authors})
+    url_for = app.url_path_for
+    rendered = template.render(authors=authors, url_for=url_for)
 
     assert "<title>Authors</title>" in rendered
     assert "Author 1 <em>0 articles</em>"
     assert "Author 2 <em>0 articles</em>"
+
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main(["-s", __file__])
