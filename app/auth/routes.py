01e63427c17141681fbbb14aed98efb5f3936709
jay ess <mentoce@gmail.com>
2023-04-15 22:03:50 -0700

    Start work on auth routes and forms

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,28 @@
-from flask import redirect, render_template
-from flask_login import current_user
+from flask import redirect, render_template, request, url_for
+from flask_login import current_user, login_user
 
-from app.auth import bp
+from app import db, models
+from app.auth import bp, forms
 
 
 @bp.route("/login", methods=["get", "post"])
 def login():
-    return "Login"
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = models.User.query.filter_by(username=form.username.data).first()
+        if not user or not user.check_password(form.password.data):
+            pass
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        return redirect(url_for("main.index"))
+    return render_template("auth/login.html", form=form)
 
 
 @bp.route("/logout")
 def logout():
+    return ""
+
+
+@bp.route("/register", methods=["get", "post"])
+def register():
     return ""
