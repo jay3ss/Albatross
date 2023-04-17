5a8e408dc68e7991e9db14b654be181b1f6df440
jay ess <mentoce@gmail.com>
2023-04-16 17:56:55 -0700

    Use proper server name

--- tests/testing_configs.py
+++ tests/testing_configs.py
@@ -5,5 +5,5 @@ class TestConfig(config.Config):
     DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    # APPLICATION_ROOT = ""
-    # SERVER_NAME = "localhost"
+    APPLICATION_ROOT = ""
+    SERVER_NAME = "localhost.localdomain"
