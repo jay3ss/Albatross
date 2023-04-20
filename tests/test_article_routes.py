4ff6fba65da0afd6bafccc92b13fffd2c7d2824e
jay ess <mentoce@gmail.com>
2023-04-19 17:33:50 -0700

    Add more tests for getting an article while authenticated or not

--- tests/test_article_routes.py
+++ tests/test_article_routes.py
@@ -1,5 +1,7 @@
 from flask import url_for
 
+from app import models
+
 
 def test_attempt_to_get_all_articles_while_not_authenticated(client):
     response = client.get(url_for("articles.articles"))
@@ -10,11 +12,56 @@ def test_attempt_to_get_all_articles_while_not_authenticated(client):
     assert "Sign in" in response.text
 
 
-def test_attempt_to_get_all_articles_while_authenticated(auth, client):
+def test_attempt_to_get_all_articles_while_authenticated(auth, client, session):
+    title = "Test Article Title"
+    content = "Test article content"
+    user = session.get(models.User, 1)
+    article = models.Article(title=title, content=content, user=user)
+    session.add(article)
+    session.commit()
+
     auth.login()
     response = client.get(url_for("articles.articles"))
+
     assert response.status_code == 200
     assert "Articles" in response.text
+    assert article.title in response.text
+    assert article.slug in response.text
+
+
+def test_get_single_article_while_authenticated(auth, client, session):
+    title = "Test Article Title"
+    content = "Test article content"
+    user = session.get(models.User, 1)
+    article = models.Article(title=title, content=content, user=user)
+    session.add(article)
+    session.commit()
+
+    auth.login()
+    response = client.get(url_for("articles.article", slug=article.slug))
+
+    assert response.status_code == 200
+    assert article.title in response.text
+    assert article.content in response.text
+
+
+def test_get_single_article_while_not_authenticated(client, session):
+    title = "Test Article Title"
+    content = "Test article content"
+    user = session.get(models.User, 1)
+    article = models.Article(title=title, content=content, user=user)
+    session.add(article)
+    session.commit()
+
+    response = client.get(url_for("articles.article", slug=article.slug))
+    assert response.status_code == 302
+
+    response = client.get(
+        url_for("articles.article", slug=article.slug),
+        follow_redirects=True
+    )
+    assert response.status_code == 200
+    assert "Sign in" in response.text
 
 
 def test_create_article(client, auth):
@@ -29,12 +76,6 @@ def test_create_article(client, auth):
     )
     assert response.status_code == 200
     assert "Test Article" in response.text
-
-
-def test_get_article(client, article):
-    response = client.get(url_for("articles.article", slug=article.slug))
-    assert response.status_code == 200
-    assert article.title in response.text
 
 
 def test_update_article(client, auth, article):
