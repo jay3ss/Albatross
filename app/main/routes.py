9cfe5b83f5154e3846cfc5cdcc4d8a44469da5a9
jay ess <mentoce@gmail.com>
2023-04-22 20:52:08 -0700

    Take care of authenticated user going to landing page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_login import current_user
 
 from app.main import bp
 
 
 @bp.route("/")
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for("articles.articles"))
     return render_template("landing_page.html")
