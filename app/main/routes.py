ec759d0324134ad491f197bca9baa7be17756885
jay ess <mentoce@gmail.com>
2023-04-23 17:05:29 -0700

    Update what to do if user is logged in and attempts to access 'main.index' route, add route for user profile

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,19 @@
 from flask import render_template, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 
+from app import db, models
 from app.main import bp
 
 
 @bp.route("/")
 def index():
     if current_user.is_authenticated:
-        return redirect(url_for("articles.articles"))
+        return redirect(url_for("main.profile", username=current_user.username))
     return render_template("landing_page.html")
+
+
+@bp.route("/u/<username>")
+@login_required
+def profile(username):
+    user = db.first_or_404(db.select(models.User).filter_by(username=username))
+    return render_template("main/profile.html", user=user)
