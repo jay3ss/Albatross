785f5db2d8622a5ae89eb9b25a86c3bf15a82478
jay ess <mentoce@gmail.com>
2023-04-25 22:06:06 -0700

    Implement route to update user profile

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for
+from flask import flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from app import db, models
-from app.main import bp
+from app.main import bp, forms
 
 
 @bp.route("/")
@@ -19,3 +19,24 @@ def profile(username):
         username_lower=username.lower())
     )
     return render_template("main/profile.html", user=user)
+
+
+@bp.route("/u/<username>/update", methods=["get", "post"])
+@login_required
+def update_profile(username):
+    form = forms.EditUserForm()
+    if form.validate_on_submit():
+        form.populate_obj(current_user)
+        if form.password.data:
+            current_user.set_password(form.password.data)
+        db.session.commit()
+        flash("Your profile has been updated!", "success")
+        return redirect(url_for("main.profile", username=current_user.username))
+
+    user: models.User = db.first_or_404(
+        db.select(models.User).filter_by(username_lower=username.lower())
+    )
+    form.username.data = user.username
+    form.email.data = user.email
+    form.about.data = user.about
+    return render_template("main/update_profile.html", form=form, user=user)
