d8870bd7709bf617273f60654d6cb21e274c2467
jay ess <mentoce@gmail.com>
2023-05-07 13:53:51 -0700

    Remove outdated app

--- albatross.py
+++
@@ -1,9 +0,0 @@
-from albatross import create_app, db, models, schemas
-
-
-app = create_app()
-
-
-@app.make_shell_context
-def make_shell_context():
-    return {"db": db, "models": models, "schemas": schemas}  # TODO: add models, etc.
