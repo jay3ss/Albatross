b571a43e25b767d624c31a8b5edf3a1c73cebb9d
jay ess <mentoce@gmail.com>
2023-05-12 20:19:55 -0700

    Add and register jinja filter for code syntax highlighting

--- app/jinja/filters.py
+++ app/jinja/filters.py
@@ -1,6 +1,9 @@
 import datetime as dt
 
 from flask import Flask
+from pygments import highlight
+from pygments.formatters import html
+from pygments.lexers import get_lexer_by_name
 
 
 def datetime_format(value: dt.datetime, format: str = "%B %-d, %Y") -> str:
@@ -22,6 +25,28 @@ def datetime_format(value: dt.datetime, format: str = "%B %-d, %Y") -> str:
     return value.strftime(format)
 
 
+def highlight_syntax(code: str, lang: str = "python", style: str = "monokai") -> str:
+    """
+    Highlights the code block with the given style
+
+    Args:
+        code (str): The code to highlight.
+        lang (str, optional): The language. Defaults to "python."
+        style (str, optional): The style to use. Defaults to "monokai."
+
+    Returns:
+        str: The code with the syntax highlighted.
+
+    Adapted from:
+    https://gist.github.com/deepns/22d366709a96f9e6fceba8abc8bdb156
+    """
+    return highlight(
+        code=code,
+        lexer=get_lexer_by_name(lang),
+        formatter=html.HtmlFormatter()
+    )
+
+
 def register_filters(app: Flask) -> Flask:
     """
     Register the custom filters
@@ -36,5 +61,6 @@ def register_filters(app: Flask) -> Flask:
     https://abstractkitchen.com/blog/how-to-create-custom-jinja-filters-in-flask/
     """
     app.jinja_env.filters["datetime_format"]: list = datetime_format
+    app.jinja_env.filters["highlight_syntax"] = highlight_syntax
 
     return app
